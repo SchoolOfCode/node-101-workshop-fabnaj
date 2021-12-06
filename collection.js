8635e5e56eca20a491beb739bc8ccd637f302657
Fabian Mash <fabianmash@gmail.com>
2021-12-06 20:00:19 +0000

    jsonplayingupagainQ3

---
+++ collection.js
@@ -0,0 +1,22 @@
+//3a) Give the array it's own file 
+
+let myCollection = [
+    {
+      name: "School of Code mug",
+      count: 1,
+      whatILike: "It has my cute pixel character on it!"
+    },
+    {
+      name: "School of Code hat",
+      count: 2,
+      whatILike: "An often overlooked fashion accessory"
+    },
+    {
+      name: "School of Code pillow",
+      count: 1,
+      whatILike: "Eat. Sleep. Code. Repeat :)"
+    }
+  ];
+
+  //3b) Change the package.json so it has the property type: module
+  export default myCollection;
--- index.js
+++ index.js
@@ -1,34 +1,28 @@
-let myCollection = [
-    {
-      name: "School of Code mug",
-      count: 1,
-      whatILike: "It has my cute pixel character on it!"
-    },
-    {
-      name: "School of Code hat",
-      count: 2,
-      whatILike: "An often overlooked fashion accessory"
-    },
-    {
-      name: "School of Code pillow",
-      count: 1,
-      whatILike: "Eat. Sleep. Code. Repeat :)"
-    }
-  ];
-  console.log(myCollection)
+//3c) Import myCollection from collection.js
 
-  function describeItem (item) {
-    console.log(item)
-  }
+import myCollection from "./collection.js";
+
+
+//2c) Create a function called describeItem
+// if only one item in collection, say "I have a name. Here's what I like about it: (whatILike).
+// if more than one item in collection, say "I have (count) names. Here's what I like about them: (whatILike)
+
+function describeItem(item){
+ if (item.count === 1){
+   console.log(`I have a ${item.name}. Here's what I like about it: ${item.whatILike}`);
+ } else {
+   console.log(`I have ${item.count} ${item.name}s. Here's what I like about them: ${item.whatILike}`);
   
+  }
+}
+//describeItem(myCollection[0]);
 
-let mug = myCollection[0];
+//2d) Make a function called describeCollection that takes in an array as a parameter.
+//It should loop throught the array, and for each item, call the describeItem function
 
-describeItem(mug)
+function describeCollection(array){
+  return array.forEach(describeItem);
+}
 
+describeCollection(myCollection);
 
-
-
-// create a function - name function
-// function needs to take in an item as ()
-//
